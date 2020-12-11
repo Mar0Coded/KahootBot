1a5c58a3827dc116ad2b1def8560043d4a7cc42f
Mar0Coded <71867245+Mar0Coded@users.noreply.github.com>
2020-12-11 09:45:15 +0100

    Added a Random Name Generator
    Now you can choose between a random name or a Custom Name.

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 import random
+import requests
 import os
 
-os.system('pip install -U KahootPY')
+os.system('pip install kahootpy')
 
 from kahoot import client
 
 bot = client()
 
-gamePin = input("What is the game pin?: ")
-user2 = input('What is your username?: ')
+text = """
+1. Custom Name
+2. Generated Name
+"""
 
-numbers = 0
+ask = input(': ')
 
-print(f'Sending bots to {gamePin}')
+if ask == '1':
+  gamePin = input("What is the game pin?: ")
+  user2 = input('What is your username?: ')
 
-while True:
-  numbers += 1
-  user = user2
-  username = user + str(numbers)
-  bot.join(gamePin,username)
-  def joinHandle():
-    pass
-  bot.on("joined",joinHandle)
+  numbers = 0
+
+  print(f'Sending bots named {user2} to {gamePin}')
+
+  while True:
+    numbers += 1
+    user = user2
+    username = user + '-' + str(numbers)
+    bot.join(gamePin,username)
+    def joinHandle():
+      pass
+    def QuestionStartCallback(question):
+      bot.answerQuestion()
+    bot.on("questionStart",QuestionStartCallback)
+
+
+elif ask == '2':
+
+  r = requests.Session()
+  game2p = input('Game ID: ')
+
+  print('Sending Bots')
+
+  while True:
+    s = r.get('https://apis.kahoot.it/namerator')
+
+    a = s.json()
+
+    b = a['name']
+
+    bot.join(game2p,b)
+    def joinHandle():
+      pass
+    def QuestionStartCallback(question):
+      bot.answerQuestion()
+    bot.on("questionStart",QuestionStartCallback)
